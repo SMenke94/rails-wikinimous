496808bab45793e5ca00af40ed9da8f48401f942
Saskia Menke <menkesaskia@web.de>
2018-08-07 12:47:51 +0200

    wiki medium pretty

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,16 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
+require 'faker'
+
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::GameOfThrones.quote
+  )
+  article.save!
+end
+
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
